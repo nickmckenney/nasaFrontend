9dd5b722ddb429576605e6cd9d3cf232593fe978
NickMckenney <mckenney2001@gmail.com>
2019-12-23 16:41:04 -0500

    added in axios with importing  url

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import { Route, Link } from "react-router-dom";
+class App extends Component {
+  componentDidMount() {
+    this.getData();
+  }
+  getData = () => {
+    axios.get("https://nasa-picutres.herokuapp.com/").then(res => {
+      this.setState({ nasa: res.data });
+    });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
-          className="App-logo"
-        />
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
+            className="App-logo"
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
